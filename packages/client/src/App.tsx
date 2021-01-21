6c47600116e7a7c069db3bb2c39e0086ca9ff0e5
Clark Tozer <clarkwtozer@gmail.com>
2021-01-21 18:05:13 +1100

    initial packages

---
+++ packages/client/src/App.tsx
@@ -0,0 +1,29 @@
+import { add } from "@hotelhero/common";
+import React, { FC } from "react";
+import "./App.css";
+import logo from "./logo.svg";
+
+const App: FC = () => {
+    console.log(add(1, 2));
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
+};
+
+export default App;
