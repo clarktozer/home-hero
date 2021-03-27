32f2f6c04fcff9fcc9b2cafc0283f44d2ad4458f
Clark Tozer <clarkwtozer@gmail.com>
2021-03-27 16:35:37 +1100

    rename

--- packages/client/src/graphql/mutations/index.ts
+++ packages/client/src/graphql/mutations/index.ts
@@ -2,4 +2,4 @@ export * from "./ConnectStripe";
 export * from "./CreateBooking";
 export * from "./DisconnectStripe";
 export * from "./HostListing";
-export * from "./Logout2";
+export * from "./Logout";
